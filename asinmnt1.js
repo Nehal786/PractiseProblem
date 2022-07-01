0ba751380e289baecd1f3fa9d88d861f9c69a937
Nehal Ahmed Ansari <nehalahmedansari0101@gmail.com>
2022-07-01 15:23:55 +0530

    Assignment 1

---
+++ asinmnt1.js
@@ -0,0 +1,20 @@
+//Create a basic calculator program using javascript
+
+let choise = 'c';
+
+switch(choise) {
+    case 'a':
+        console.log(5 + 5);
+        break;
+    case 'b':
+        console.log(20 - 5);
+        break;
+    case 'c':
+        console.log(15 * 5);
+        break;
+    case 'd':
+        console.log(30 / 3);
+        break;
+    default:
+        console.log("Invalid Choise");
+}
